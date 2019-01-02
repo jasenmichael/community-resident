152a898280c597c5c713b292a7c1e87c038fac1e
jasenmichael <jasen@ready-builder.com>
2019-01-02 17:15:42 -0600

    add install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# clone backend
+# symlink backend/public/api to root hosted folder - /api
+
+
+# clone frontend
+# install dependences npm i
+# build frontend npm run build
+# cp the dist folder to the root hosted folder - /
+
+# config database
